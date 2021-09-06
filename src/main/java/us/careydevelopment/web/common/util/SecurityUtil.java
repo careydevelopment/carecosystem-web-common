6acf0e329f7ef7643adc09c48afb6dff658a66cd
careydevelopment <mrbrianmcarey@gmail.com>
2021-09-06 18:22:33 -0400

    work for admin security

--- src/main/java/us/careydevelopment/web/common/util/SecurityUtil.java
+++ src/main/java/us/careydevelopment/web/common/util/SecurityUtil.java
@@ -1,10 +1,15 @@
 package us.careydevelopment.web.common.util;
 
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import us.careydevelopment.web.common.constants.Authority;
 
 @Component
 public class SecurityUtil {
@@ -13,9 +18,12 @@ public class SecurityUtil {
 
     public static final String ANONYMOUS_USER_NAME = "anonymousUser";
     
+    private static final List<String> VALID_ADMIN_AUTHORITIES = List.of(Authority.ADMIN_ECOSYSTEM_USER);
+    
+    
     @Autowired
     private JwtUtil jwtUtil;
-        
+    
     
     /**
      * Here, jwtToken was retrieved via @Cookie
@@ -37,5 +45,30 @@ public class SecurityUtil {
         }
 
         return user;
+    }
+    
+    
+    public boolean isAdmin(String jwtToken) {
+        boolean isAdmin = false;
+        
+        try {
+            List<String> authorities = jwtUtil.getAuthoritiesFromToken(jwtToken);
+            LOG.debug("User authorities is " + authorities);
+            
+            if (authorities != null) {
+                for (String auth : VALID_ADMIN_AUTHORITIES) {
+                    if (authorities.contains(auth)) {
+                        LOG.debug("User is admin");
+                        
+                        isAdmin = true;
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("Problem trying to retrieve authorities for user!", e);
+        }
+        
+        return isAdmin;
     }
 }
